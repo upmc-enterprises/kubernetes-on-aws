a3fab55b677775f7148e1b84dac3fad488e46c23
Steve Sloka <slokas@upmc.edu>
2017-08-01 13:52:01 -0400

    Modify cert to allow RBAC to assign role

--- scripts/certs/generate-worker-certs.sh
+++ scripts/certs/generate-worker-certs.sh
@@ -1,4 +1,4 @@
 # Generate Worker Certs
 openssl genrsa -out worker-key.pem 2048
-openssl req -new -key worker-key.pem -out worker.csr -subj "/CN=worker.k8s.cluster.internal" -config worker-openssl.cnf
+openssl req -new -key worker-key.pem -out worker.csr -subj "/CN=worker.k8s.local/O=system:nodes" -config worker-openssl.cnf
 openssl x509 -req -in worker.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out worker.pem -days 365 -extensions v3_req -extfile worker-openssl.cnf
